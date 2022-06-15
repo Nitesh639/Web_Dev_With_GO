a455c502083ea07389d6bb69951c795cfddb012a
Nitesh639 <nkush9598@gmail.com>
2022-06-15 16:25:55 +0530

    passing data in templates

---
+++ Templates/Data_struct_passing/slice/slice.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+func main(){
+	tpl := template.Must(template.ParseFiles("index.html"))
+
+	sl := []int{1,2,3,4,5,6,7,8}
+
+	_ = tpl.ExecuteTemplate(os.Stdout,"index.html",sl)
+}
